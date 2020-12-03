994997831e3ee11db9e50340916a0effcf3300d0
june8050 <75426342+june8050@users.noreply.github.com>
2020-12-03 22:14:19 +0900

    Update Server.py

--- Server.py
+++ Server.py
@@ -15,6 +15,63 @@ from Time import Time
 httpd = None
 DISPLAY = 'DISPLAY' in environ
 
+def select_white(image, white):
+    lower = np.uint8([white,white,white])
+    upper = np.uint8([255,255,255])
+    white_mask = cv2.inRange(image, lower, upper)
+    return white_mask
+
+def first_nonzero(arr, axis, invalid_val=-1):
+    arr = np.flipud(arr)
+    mask = arr!=0
+    return np.where(mask.any(axis=axis), mask.argmax(axis=axis), invalid_val)
+
+def set_path3(image, forward_criteria):
+    height, width = image.shape
+    height = height-1
+    width = width-1
+    center=int(width/2)
+    left=0
+    right=width
+    
+    center = int((left+right)/2)        
+    
+    try:
+        '''if image[height][:center].min(axis=0) == 255:
+            left = 0
+        else:
+            left = image[height][:center].argmin(axis=0)    
+        if image[height][center:].max(axis=0) == 0:
+            right = width
+        else:    
+            right = center+image[height][center:].argmax(axis=0)  
+            q
+        center = int((left+right)/2)'''  
+        
+        print(int(first_nonzero(image[:,center],0,height)))
+        forward = min(int(height),int(first_nonzero(image[:,center],0,height))-1)
+        #print(height, first_nonzero(image[:,center],0,height))
+        
+        left_line = first_nonzero(image[height-forward:height,center:],1, width-center)
+        right_line = first_nonzero(np.fliplr(image[height-forward:height,:center]),1, center)
+        
+        center_y = (np.ones(forward)*2*center-left_line+right_line)/2-center
+        center_x = np.vstack((np.arange(forward), np.zeros(forward)))
+        m, c = np.linalg.lstsq(center_x.T, center_y, rcond=-1)[0]
+        if forward < 20 or forward < 50 and abs(m) < 0.35:
+            result = 'backward'
+        elif abs(m) < forward_criteria:
+            result = 'forward'
+        elif m > 0:
+            result = 'left'
+        else:
+            result = 'right'
+    except:
+        result = 'backward'
+        m = 0
+    
+    return result, round(m,4), forward
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -49,7 +106,16 @@ class Handler(BaseHTTPRequestHandler):
             data = np.asarray(bytearray(data), dtype="uint8")
             img = cv2.imdecode(data, cv2.IMREAD_ANYCOLOR)
             #Marker
-            cv2.imshow('image', img)
+            masked_image = select_white(img, 160)
+            result=set_path3(masked_image,0.25)
+            #result=set_path1(masked_image,120)
+            y1, x1 = masked_image.shape
+            x1 = int(x1/2)
+            x2 = int(-result[2] * result[1] + x1)
+            y2 = y1-result[2] 
+            cv2.line(masked_image,(x1,y1),(x2,y2),(255),2)
+            cv2.imshow("Processed", masked_image)
+            #cv2.imshow('image', img)
             cv2.waitKey(1)
 
         else:
