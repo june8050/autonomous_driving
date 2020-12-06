f438fdee2ce3b5e5e440fa8071b7cb16bbf9b6c2
june8050 <75426342+june8050@users.noreply.github.com>
2020-12-06 17:14:04 +0900

    Update Client.py

--- Client.py
+++ Client.py
@@ -3,7 +3,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import picamera
 from sys import argv
-from Time import Time
+from time import time
 import cv2
 import numpy as np
 from time import sleep
@@ -14,17 +14,9 @@ PORT = 8000
 file = 'steveholt.jpg'
 host = 'localhost'
 
-
 print(argv)
 
-HALF=.5
-MOTOR_SPEEDS = {
-    "c": [0, 100, 0, 50], "x": [0, 100, 0, 100], "z": [0, 50, 0, 100],
-    "a": [0, 100, 100, 0], "s": [100, 100, 100, 100], "d": [100, 0, 0, 100],
-    "e": [100, 0, 50, 0], "w": [100, 0, 100, 0], "q": [50, 0, 100, 0],
-}
-
-# pin definition
+ # pin definition
 motor1A = 16
 motor1B = 18
 motor2A = 22
@@ -32,10 +24,10 @@ motor2B = 24
 
 # pin setting
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(motor1A,GPIO.OUT)
-GPIO.setup(motor1B,GPIO.OUT)
-GPIO.setup(motor2A,GPIO.OUT)
-GPIO.setup(motor2B,GPIO.OUT)
+GPIO.setup(motor1A, GPIO.OUT)
+GPIO.setup(motor1B, GPIO.OUT)
+GPIO.setup(motor2A, GPIO.OUT)
+GPIO.setup(motor2B, GPIO.OUT)
 
 # PWM
 p1A = GPIO.PWM(motor1A, 1000)
@@ -46,6 +38,14 @@ p1A.start(0)
 p1B.start(0)
 p2A.start(0)
 p2B.start(0)
+    
+HALF = .5
+MOTOR_SPEEDS = {
+    "c": [0, 100, 0, 50], "x": [0, 100, 0, 100], "z": [0, 50, 0, 100],
+    "a": [0, 100, 100, 0], "s": [100, 100, 100, 100], "d": [100, 0, 0, 100],
+    "e": [100, 0, 50, 0], "w": [100, 0, 100, 0], "q": [50, 0, 100, 0],
+}
+
 
 def Upload(body, headers={}):
     conn = HTTPConnection(f"{argv[1] if len(argv) > 1 else  'localhost'}:{PORT}")
@@ -54,8 +54,8 @@ def Upload(body, headers={}):
 
     print(res.getheaders())
     print(res.getheader('X-Server2Client', 'Fallback'))
-    print(res.read())
     print('Uploaded to', host, 'with status', res.status)
+    return res.read()
 
 
 def Camera():
@@ -78,12 +78,30 @@ def UploadCamera(camera, rawCapture):
             raise Exception('Image encode error')
 
         rawCapture.truncate(0)
-        Upload(img.tobytes())
+        data = Upload(img.tobytes())
+        data = json.loads(data)
+        print(time(), data)
+        #action = data['action']
+        #print('action', action)
+
+        direction = MOTOR_SPEEDS[data]
+
+        a = direction[0]
+        b = direction[1]
+        c = direction[2]
+        d = direction[3]
+
+        p1A.ChangeDutyCycle(a)
+        p1B.ChangeDutyCycle(b)
+        p2A.ChangeDutyCycle(c)
+        p2B.ChangeDutyCycle(d)
 
 
 def main():
     camera = Camera()
     rawCapture = PiRGBArray(camera, size=(320, 240))
+
+    
 
     while True:
         try:
@@ -92,26 +110,6 @@ def main():
         except ConnectionRefusedError as error:
             print(error)
             sleep(1)
-                          
-        res = conn.getresponse()
-        chunk = res.readline()
-        if (chunk == b'\n'): continue
-        if (not chunk): break
 
-        chunk = chunk[:-1].decode()
-        data = json.loads(chunk)
-        print(Time(), data)
-        action = data['action']
-        print('action', action)
-        direction = MOTOR_SPEEDS[action]
-        a=direction[0]
-        b=direction[1]
-        c=direction[2]
-        d=direction[3]
-        p1A.ChangeDutyCycle(a)
-        p1B.ChangeDutyCycle(b)
-        p2A.ChangeDutyCycle(c)
-        p2B.ChangeDutyCycle(d)
-        
 
 main()
