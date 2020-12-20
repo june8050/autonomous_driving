c9a842ba3cb4cad4fe9c2c33d20a8a7f5f3ebbcf
june8050 <75426342+june8050@users.noreply.github.com>
2020-12-20 17:27:02 +0900

    Update Server.py

--- Server.py
+++ Server.py
@@ -45,11 +45,11 @@ def cascade(img):
     
     if (x+w)*(y+h) > 100: #사각형이 어느정도 크기 이상이면
         key = 's'
-	self.wfile.write(bytes(json.dumps(key), encoding='utf8'))
-	self.wfile.write(b'\n')
-	'''cv2.waitKey(1)'''
-	time.sleep(5)
-	key = 'w'
+        self.wfile.write(bytes(json.dumps(key), encoding='utf8'))
+        self.wfile.write(b'\n')
+        '''cv2.waitKey(1)'''
+        time.sleep(5)
+        key = 'w'
     else:
         key = 'blank'	
     
