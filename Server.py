2512861039ba475891d979121f4a1fd0d4518b50
june8050 <75426342+june8050@users.noreply.github.com>
2020-12-20 18:04:47 +0900

    Update Server.py

--- Server.py
+++ Server.py
@@ -61,7 +61,7 @@ def marker(img):
         print('detected',marker.id)
         marker.highlite_marker(img)
         
-    if marker.id == 144:
+    if markers.id == 144:
         key = 'a'
         self.wfile.write(bytes(json.dumps(key), encoding='utf8'))
         self.wfile.write(b'\n')
@@ -72,7 +72,7 @@ def marker(img):
         self.wfile.write(b'\n')
         '''cv2.waitKey(1)'''
         time.sleep(2)#전진시간
-    elif marker.id == 922:
+    elif markers.id == 922:
         key = 'd'
         self.wfile.write(bytes(json.dumps(key), encoding='utf8'))
         self.wfile.write(b'\n')
@@ -83,7 +83,7 @@ def marker(img):
         self.wfile.write(b'\n')
         '''cv2.waitKey(1)'''
         time.sleep(2)#전진시간
-    elif marker.id == 2537:
+    elif markers.id == 2537:
         key = 's'
     else:
         key = 'blank'
